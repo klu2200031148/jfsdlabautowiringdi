7b7f3752bf11343d09f1e5fc8a820703be9c30f2
klu2200031148 <2200031148@kluniversity.in>
2024-12-07 10:50:31 +0530

    V1.0

---
+++ src/main/java/com/klef/jfsd/exam/spring_di_autowiring/App.java
@@ -0,0 +1,13 @@
+package com.klef.jfsd.exam.spring_di_autowiring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
